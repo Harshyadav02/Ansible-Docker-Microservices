28426efd2dc0b8cc5363560cd310a82b6edc7840
harshyadav02 <harshyadav5736@gmail.com>
2025-01-03 17:03:02 +0530

    entry point for application

---
+++ gatway/src/main/java/com/shop/gatway/GatwayApplication.java
@@ -0,0 +1,18 @@
+package com.shop.gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatwayApplication.class, args);
+	}
+
+}
+
+
